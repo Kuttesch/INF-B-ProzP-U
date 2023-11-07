991b3f0cd5cb3291d7a68043a375461c04630418
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2023-11-07 10:14:39 +0100

    initial Push

---
+++ zahltab/cmake-build-debug/CMakeFiles/zahltab.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zahltab.dir/zahltab.c.obj"
+  "CMakeFiles/zahltab.dir/zahltab.c.obj.d"
+  "libzahltab.dll.a"
+  "zahltab.exe"
+  "zahltab.exe.manifest"
+  "zahltab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zahltab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zahltab/cmake-build-debug/CMakeFiles/zahltab.dir/zahltab.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/zahltab.dir/zahltab.c.obj: \
+ D:\Github\INF-B-ProzP-U\02-zahltab\zahltab\zahltab.c \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/math.h \
+ E:/CLion\ 2023.2.2/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h
