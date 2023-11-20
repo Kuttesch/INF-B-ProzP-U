e554372f6843e7bff10dc9ffea6cb8a0fb9c9700
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2023-11-20 14:34:45 +0100

    GEnau

---
+++ 06-bruchrechnung/bruchrechnung/rational.h
@@ -0,0 +1,19 @@
+#ifndef BRUCHRECHNUNG_RATIONAL_H
+#define BRUCHRECHNUNG_RATIONAL_H
+
+struct bruch_s {
+    int zaehler;
+    int nenner;
+};
+
+struct bruch_s add(struct bruch_s bruch1, struct bruch_s bruch2);
+struct bruch_s sub(struct bruch_s bruch1, struct bruch_s bruch2);
+struct bruch_s mul(struct bruch_s bruch1, struct bruch_s bruch2);
+struct bruch_s div(struct bruch_s bruch1, struct bruch_s bruch2);
+
+
+struct bruch_s kuerzen(struct bruch_s bruch);
+
+int ggT(int a, int b);
+
+#endif //BRUCHRECHNUNG_RATIONAL_H
---
+++ Challenge/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*int main() {
+    char userinput[50];
+    printf("Give me String");
+    scanf("%s",userinput);
+
+    printf("%s", strrev(userinput));
+    return 0;
+}
+*/
