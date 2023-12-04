3fbfeb757af152746e2a2d74b6b9a6fc582e3be9
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2023-12-04 18:18:40 +0100

    Gud Cod

--- 06-bruchrechnung/bruchrechnung/cmake-build-debug/CMakeFiles/bruchrechnung.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bruchrechnung.
-# This may be replaced when dependencies are built.
--- 06-bruchrechnung/bruchrechnung/cmake-build-debug/CMakeFiles/bruchrechnung.dir/rational.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/bruchrechnung.dir/rational.c.obj: \
- D:\Github\INF-B-ProzP-U\06-bruchrechnung\bruchrechnung\rational.c \
- D:\Github\INF-B-ProzP-U\06-bruchrechnung\bruchrechnung\rational.h
